185e69505272c8ef64302bdfb70f7143ee58d9a1
Kuslam <kusaluprety@gmail.com>
2024-05-07 21:55:06 -0700

    Update dynamics.py
    added some basic code

--- src/dynamics.py
+++ src/dynamics.py
@@ -3,6 +3,17 @@ import numpy as np
 # TODO: Kusal to implement how the state evolves at every step 
 # starting with a single virtual creature in a simple aerodynamic
 # environment/model
+
+def get_aero_data(airfoil):
+    # airfoil_database parameters:
+    # 1) dCL/dAlpha
+    # 2) angle of min stall (deg)
+    # 3) angle of max stall (deg)
+    # 4) cD0
+    airfoil_database = {
+        "NACA 0012": [0.11667, -8, 8, 0.02]
+    }
+
 def forward_step(virtual_creature):
     """
     Takes a virtual creature and updates its state by one step based on
